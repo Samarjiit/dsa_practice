60867c53e965c52076181296bf7023f278563e20
samarjit mahi <41121431+Samarjiit@users.noreply.github.com>
2023-11-12 12:40:56 +0530

    Time: 3 ms (66.67%), Space: 6.4 MB (33.33%) - LeetHub

---
+++ 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+   long long dp[51][51];
+    long long solve(int i,int n,int limit){
+        if(i==3){
+            if(n==0)return 1;
+            return 0;
+        }
+        if(n==0)return 1;
+
+        if(dp[i][n]!=-1)return dp[i][n];
+
+        long long ans=0;
+        for(int candy=0;candy<=min(n,limit);candy++){
+            ans+=solve(i+1,n-candy,limit);
+        }
+        return dp[i][n]=ans;
+    }
+    int distributeCandies(int n, int limit) {
+        memset(dp,-1,sizeof(dp));
+            return solve(0,n,limit);
+    }
+};
